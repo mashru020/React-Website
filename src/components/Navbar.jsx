97000274a1b0ff1c0293e9583ed06b8c46e32e7c
mashru020 <mashrurrahman16@gmail.com>
2022-10-01 18:37:15 +0600

    globall css and navbar done (1.15)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,33 @@
+import { useState } from 'react';
 import {Link, NavLink} from 'react-router-dom';
 import { links } from '../data';
 import {GoThreeBars} from 'react-icons/go'
+import {MdOutlineClose} from 'react-icons/md'
+
 import './navbar.css'
 import Logo from '../images/logo.png'
 
 const Navbar = () => {
+    const [isNavShowing, setIsNavShowing] = useState(false);
   return (
     <nav>
         <div className="container nav__container">
-            <Link to='/' className='logo'>
+            <Link to='/home' className='logo'>
                 <img src={Logo} alt="Nav logo" />
             </Link>
-            <ul className='nav__links'>
+            <ul className={`nav__links ${isNavShowing ? 'show__nav' : 'hide__nav' }`}>
                {links.map(({name, path},index) => {
                 return(
-                    <li>
-                        <NavLink to={path}>{name}</NavLink>
+                    <li key={index}>
+                        <NavLink to={path} className={({isActive}) => isActive ? 'active-nav' : ''}>{name}</NavLink>
                     </li>
                 )
                })}
             </ul>
-            <button className='nav__toggle-btn'>
-                <GoThreeBars/>
+            <button className='nav__toggle-btn' onClick={() => setIsNavShowing(!isNavShowing)}>
+                {
+                    isNavShowing ? <MdOutlineClose/> : <GoThreeBars/>
+                }
             </button>
         </div>
     </nav>
